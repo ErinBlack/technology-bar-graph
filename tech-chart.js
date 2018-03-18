c1a66dae6a8728d8ae629c7c0ffa8f63f35f7124
Erin Black <erinblackdesign@gmail.com>
2018-03-18 17:57:51 -0500

    got y extent logging correctly

--- tech-chart.js
+++ tech-chart.js
@@ -1,32 +1,72 @@
 // JS closure
 (function(){
+  var height = 800,
+      width = 500;
+  var padding = 50;
+
+  var chart = d3.select('.chart__section')
+    .append('svg')
+    .attr('height', height + padding * 2 )
+    .attr('width', width + padding * 2)
+    .attr('class', 'chart__body')
+    .append('g');
+
+  var yScale = d3.scale.linear()
+                        .range([height, 0]);
+
+  var xScale = d3.time.scale()
+                        .range([0, width]);
+
+  // Set up the x axis
+  var xAxis = d3.svg.axis().scale(xScale)
+                            .orient("bottom")
+                            .ticks(8);
+  // Set up the y axis
+  var yAxis = d3.svg.axis().scale(yScale)
+                            .orient("left")
+                            .ticks(20);
+
   var currentYear = (new Date()).getFullYear() + 1;
-  var chart = d3.select('.chart__body');
+
+
   // Getting chart data
   d3.csv('technologies.csv', function(data){
+    var dataLength = data.length;
     // getting year learned and parsing out the number
     data.forEach(function(d){
       d.yearLearned = parseInt(d.YEAR_LEARNED);
     });
 
+    // determining range of y axis
+    yDomain = d3.extent(data, function(d){
+        return parseInt(currentYear - d.YEAR_LEARNED);
+      });
+    console.log('yDomain', yDomain);
+
+
     // Calculating heighest value and making it 100% of chart width
-    var maxVal = d3.max(data, function(d){
+    var maxVal = d3.extent(data, function(d){
       var yearsKnown = currentYear - d.yearLearned;
       return yearsKnown;
     });
 
     // Creating reference to Bar
-    var bar = chart.selectAll('div')
+    var bar = chart.selectAll('rect')
       .data(data)
       .enter()
-      .append('div')
+      .append('rect')
       .attr('class', 'chart__bar')
-      .style('height', function(d){
-        var yearsKnown = currentYear - d.yearLearned;
-        console.log('d.yearLearned', d.yearLearned);
-        console.log('maxVal', maxVal);
-        return Number((yearsKnown/maxVal) *100) + '%';
-      });
+      .attr('width', '30px')
+      .attr('x', function(d, i){
+          var barSections = width / dataLength;
+          var barPlace = i * barSections;
+          return barPlace;
+      } )
+      .style('fill', 'blue')
+      // .attr('height', function(d){
+      //   var yearsKnown = currentYear - d.yearLearned;
+      //   return Number((yearsKnown/maxVal) *100);
+      // });
   });
 
 })();
